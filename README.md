ece5e1ce027147c6b324ccb75c9c2afc9f1b7668
Timotheus Pokorra <tp@tbits.net>
2018-01-26 15:48:55 +0100

    add logging add instructions which packages are required

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BOF
 Birds of a feather flock together: this is a software that can be used for proposing BOF topics and for voting on them
+
+# Installation
+
+on Ubuntu 14.04, you will need these packages:
+
+    apt install apache2 libapache2-mod-php7.0 php php-mysql mariadb-server php-zip git vim ca-certificates composer php-dom php-mbstring
+
+get the dependancies with composer:
+
+    cd src
+    composer install
+
+setup the logs directory
+
+    mkdir src/logs
+    chmod a+w src/logs
--- src/public/index.php
+++ src/public/index.php
@@ -5,9 +5,19 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
+    $this->logger->addInfo('Something interesting happened: we said hello to '.$name);
 
     return $response;
 });
