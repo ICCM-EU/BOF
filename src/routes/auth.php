9e562329f54a5ecac483ad9ebde2aeed25376ab3
Pim van Stam <pim@svsnet.nl>
2018-01-31 11:57:13 +0100

    Register link added

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -9,6 +9,10 @@ $app->get('/login', function (Request $request, Response $response, array $args)
     return $this->view->render($response, 'login.html');
 })->setName('login');
 
+$app->get('/register', function (Request $request, Response $response, array $args) {
+    return $this->view->render($response, 'register.html');
+})->setName('register');
+
 $app->post('/authenticate', 'ICCM\BOF\Auth:authenticate')->setName('authenticate');
 
 $app->get('/logout', 'ICCM\BOF\Auth:logout')->setName('logout');
