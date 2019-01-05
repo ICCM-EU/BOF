d0b0cd16f27bbad35a0a4fe8c4ad6ef6804b1452
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-01-05 13:41:05 +0000

    tests: define array with votes

--- cypress/integration/voting.js
+++ cypress/integration/voting.js
@@ -12,7 +12,7 @@ function logout_user() {
 function cast_vote(topic, vote) {
   cy.visit("/topics")
   // select the div class=topic
-  cy.get('div[class=topic]:nth-child(' + topic+ ')').within(() => {
+  cy.get('div[class=topic]:nth-child(' + (topic + 1) + ')').within(() => {
 
     // select the form to click
     if (vote == 0) {
@@ -32,15 +32,49 @@ function cast_vote(topic, vote) {
 
 describe('test voting stage', function() {
   it('cast votes', function() {
+    var votes_full = [
+       // topic 1
+       [1,2,3,4,5,6,7,8,9],
+       // topic 2
+       [1,2,3,4,5,6,7,8,9,10,11,12,13],
+       // topic 3
+       [1,2,3,4,5,6,7],
+       // topic 4
+       [10,11,12,13,14,15]
+    ]
+    var votes_facilitator = [
+       // topic 1
+       [21],
+       // topic 2
+       [22],
+       // topic 3
+       [22],
+       // topic 4
+       [23]
+    ]
+    var votes_quarter = [
+       // topic 1
+       [30,31,32,33,34,35,36,37],
+       // topic 2
+       [30,31,32],
+       // topic 3
+       [33,34,35,36],
+       // topic 4
+       [35,36,37,38,39]
+    ]
     var i
-    for(i=1; i < 2; i++) {
+    for(i=1; i < 60; i++) {
       login_user("user" + i, "pwd" + i)
 
-      if (i == 1) {
-        cast_vote(4, 0)
-        cast_vote(5, 1)
-        cast_vote(6, 1)
-        cast_vote(7, 2)
+      var topic
+      for (topic=1; topic < 4; topic++)
+      {
+        if (votes_facilitator[topic-1].indexOf(i) > -1)
+          cast_vote(topic, 0)
+        if (votes_full[topic-1].indexOf(i) > -1)
+          cast_vote(topic, 1)
+        if (votes_quarter[topic-1].indexOf(i) > -1)
+          cast_vote(topic, 2)
       }
       logout_user()
     }
