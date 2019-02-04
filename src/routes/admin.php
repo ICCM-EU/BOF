9881373a9d927b8b005118617cd54f268e3df2cd
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-02-04 15:35:31 +0000

    add admin page for setting the times for nomination and voting stage fixes #15

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -3,10 +3,10 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/admin', function (Request $request, Response $response, array $args) {
-    return $this->view->render($response, 'admin.html', [
-        'loggedin' => True
-    ]);
-})->setName('admin');
+require __DIR__.'/../classes/Admin.php';
+
+$app->get('/admin', 'ICCM\BOF\Admin:showAdminView')->setName('admin');
+
+$app->post('/admin', 'ICCM\BOF\Admin:update_config')->setName('admin_config');
 
 ?>
