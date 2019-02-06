9fdcc18dd59fb1d34790e0aac2a47631b0fe694b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-02-06 06:40:46 +0000

    show github fork me only on the login page

--- src/routes/home.php
+++ src/routes/home.php
@@ -6,7 +6,8 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->get('/', function (Request $request, Response $response, array $args) {
     if($request->getAttribute('userid') !== NULL)
         return $response->withRedirect($this->router->pathFor("topics"))->withStatus(302);
-    return $this->view->render($response, 'home.html');
+    $config['show_githubforkme'] = true;
+    return $this->view->render($response, 'home.html', $config);
 })->setName('home');
 
 ?>
