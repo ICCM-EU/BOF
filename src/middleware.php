66aacb7477fc49618825b0e3493240b413355217
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-02-11 22:20:37 +0000

    redirect if we are not logged in. updating slim composer packages. fixes #68

--- src/middleware.php
+++ src/middleware.php
@@ -9,11 +9,7 @@ $app->add(new \Slim\Middleware\JwtAuthentication([
     #"passthrough" => ["/home", "/login", "/authenticate"],
     "secret" => $settings['settings']['secrettoken'],
     "error" => function ($request, $response, $arguments) {
-        $data["status"] = "error";
-        $data["message"] = $arguments["message"];
-        return $response
-            ->withHeader("Content-Type", "application/json")
-            ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response->withRedirect("/?message=invalid_login")->withStatus(302);
     }
 ]));
 
