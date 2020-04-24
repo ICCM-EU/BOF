13761e62dd071853dbc53a863204c59e302ff1d7
Thomas Francis Jr <franc@tcsaf.com>
2020-04-24 12:19:10 -0400

    Added unit tests for Results class
    Added phpunit and prophecy to composer for dev environments.
    Added phpunit.xml to define tests
    Added new unit test classes in TestResults.php, TestDBO.php, and
    TestLogger.php.
    Updated .gitignore to ignore the results and code coverage files
    produced by phpunit.
    
    To run the unit tests:
    
    	cd <TOP LEVEL>/src
    	./vendor/bin/phpunit
    
    OR:
    
    	cd <TOP LEVEL>
    	./src/vendor/bin/phpunit -c src/phpunit.xml
    
    To view test results in HTML format, open src/test/results/index.html in a
    web browser.
    
    To view code coverage and other metrics, open
    src/test/results/coverage/index.html in a web browser.

---
+++ src/test/classes/TestLogger.php
@@ -0,0 +1,147 @@
+<?php
+
+require_once __DIR__ . '/../../classes/Logger.php';
+
+use PHPUnit\Framework\TestCase;
+use ICCM\BOF\Logger;
+
+/**
+ * @covers ICCM\BOF\Logger
+ */
+class TestLogger extends TestCase
+{
+    /**
+     * @covers ICCM\BOF\Logger::clearLog
+     * @test
+     */
+    public function clearLogReturnsEmpty() {
+        $logger = new Logger();
+        $logger->log('Test message');
+        $logger->clearLog();
+        $this->assertEmpty($logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::getLog
+     * @test
+     */
+    public function getLogReturnsEmptyIfNothingLogged() {
+        $logger = new Logger();
+        $this->assertEmpty($logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::getLog
+     * @test
+     */
+    public function getLogReturnsLoggedMessage() {
+        $message = 'Test message';
+        $expected = $message."\n";
+        $logger = new Logger();
+        $logger->log($message);
+        $this->assertEquals($expected, $logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::getLog
+     * @test
+     */
+    public function getLogReturnsEmptyAfterGetLog() {
+        $message = 'Test message';
+        $expected = $message."\n";
+        $logger = new Logger();
+        $logger->log($message);
+        $logger->getLog();
+        $this->assertEmpty($logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::log
+     * @test
+     */
+    public function logAppendsWithNewlines() {
+        $message1 = 'Test message';
+        $message2 = 'Test message';
+        $expected = $message1."\n".$message2."\n";
+        $logger = new Logger();
+        $logger->log($message1);
+        $logger->log($message2);
+        $this->assertEquals($expected, $logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::logBookWorkshop
+     * @test
+     */
+    public function logBookWorkshop() {
+        $dbo = $this->getMockBuilder(DBO::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getLocationNames', 'getRoundNames'])
+            ->getMock();
+
+        $dbo->expects($this->once())
+            ->method('getLocationNames')
+            ->willReturn(['one', 'two', 'three']);
+
+        $dbo->expects($this->once())
+            ->method('getRoundNames')
+            ->willReturn(['one', 'two', 'three']);
+
+        $reason = "My reason";
+
+        $logger = new Logger();
+
+        $expected = "Putting workshop '1' in round 'one' at location 'three'. Reason: {$reason}\n";
+        $logger->logBookWorkshop($dbo, 1, 0, 2, $reason);
+        $this->assertEquals($expected, $logger->getLog());
+
+        // Calling logBookWorkshop() a second time should not cause
+        // $dbo->getLocationNames() or $dbo->getRoundsNames() to be called more
+        // than once.
+
+        $expected = "Putting workshop '2' in round 'two' at location 'one'. Reason: {$reason}\n";
+        $logger->logBookWorkshop($dbo, 2, 1, 0, $reason);
+        $this->assertEquals($expected, $logger->getLog());
+    }
+
+    /**
+     * @covers ICCM\BOF\Logger::logSwitchedWorkshops
+     * @test
+     */
+    public function logSwitchedWorkshops() {
+        $dbo = $this->getMockBuilder(DBO::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getLocationNames', 'getRoundNames'])
+            ->getMock();
+
+        $dbo->expects($this->once())
+            ->method('getLocationNames')
+            ->willReturn(['one', 'two', 'three']);
+
+        $dbo->expects($this->once())
+            ->method('getRoundNames')
+            ->willReturn(['one', 'two', 'three']);
+
+        $workshop1 = 1;
+        $round1 = 0;
+        $location1 = 2;
+
+        $workshop2 = 2;
+        $round2 = 1;
+        $location2 = 0;
+
+        $logger = new Logger();
+
+        $expected = "Switched workshops!  '{$workshop1}' is now in round 'one' at location 'three'. '{$workshop2}' is now in round 'two' at location 'one'.\n";
+        $logger->logSwitchedWorkshops($dbo, $workshop1, $round1, $location1, $workshop2, $round2, $location2);
+
+        $this->assertEquals($expected, $logger->getLog());
+
+        // Calling logSwitchedWorkshops() a second time should not cause
+        // $dbo->getLocationNames() or $dbo->getRoundsNames() to be called more
+        // than once.
+        $expected = "Switched workshops!  '{$workshop1}' is now in round 'two' at location 'one'. '{$workshop2}' is now in round 'one' at location 'three'.\n";
+        $logger->logSwitchedWorkshops($dbo, $workshop1, $round2, $location2, $workshop2, $round1, $location1);
+        $this->assertEquals($expected, $logger->getLog());
+    }
+}
